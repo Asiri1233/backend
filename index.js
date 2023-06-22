a87f9628bf774747a4be85cd5723b0cff20a7146
asiri <asiribr@gmail.com>
2023-06-23 01:42:32 +0530

    fixed env files

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 
+dotenv.config()
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb" , extended: true}))
