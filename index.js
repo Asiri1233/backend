763ed04ea33fb9e4d8aa3c397eede55dfa162313
asiri <asiribr@gmail.com>
2023-06-23 01:29:37 +0530

    second commit cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb" , extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb" , extended: true}));
 app.use(cors({
-    origin: 'https://64949afdb82df90305fb5660--fantastic-frangollo-1a4fa9.netlify.app/'
+    origin: 'https://pixelmp.netlify.app/'
 }));
 
 app.use('/posts', postRoutes);
